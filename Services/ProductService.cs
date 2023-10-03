9bda0136dc9f8ea0e37c80efffe34ef870bc8c43
Sanciara Nunes <sanciara.nunes@grupoaguasdobrasil.com.br>
2023-10-03 16:24:20 -0300

    Coreção erro de paginação

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -14,8 +14,22 @@ namespace ProvaPub.Services
 
 		public ProductList  ListProducts(int page)
 		{
-			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.ToList() };
-		}
+            int pageSize = 10;
+            var totalProducts = _ctx.Products.Count();
+            var products = _ctx.Products
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var hasNext = (page * pageSize) < totalProducts;
+
+            return new ProductList()
+            {
+                HasNext = hasNext,
+                TotalCount = totalProducts,
+                Products = products
+            };
+        }
 
 	}
 }
